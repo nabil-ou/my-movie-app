32c6174ed071a3cc3fe11f07bf2b484b68a7a137
nabiloutaous <nabil.outaous@gmail.com>
2023-11-30 11:20:40 +0100

    Implement Error Handling Mechanism

---
+++ src/app/error.js
@@ -0,0 +1,11 @@
+"use client"
+
+const Error = () => {
+    return (
+        <div className="error">
+            <h1>Erreur</h1>
+            <p>Oups, une erreur s&apos;est produite.</p>
+        </div>
+    )
+}
+export default Error
---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="error">
+            <h1>404</h1>
+            <p>Oups, la page que vous recherchez n&apos;existe pas.</p>
+        </div>
+    )
+}
+export default NotFound
