e277090c4c80aeca1128203b3472663d499d674e
nabiloutaous <nabil.outaous@gmail.com>
2023-12-06 14:25:51 +0100

    feat: Added buttons to showcase movie genres

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Popular from "@/components/popular/Popular";
 import styles from "./page.module.css";
+import Genres from "@/components/Genres/Genres";
 
 export default function Home() {
     return (
         <div className={styles.main}>
             <Popular />
+            <Genres />
         </div>
     );
 }
